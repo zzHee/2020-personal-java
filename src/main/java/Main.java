7360d4bd398257795e2b176d19cc794d097c1dda
zzHee <772575675@qq.com>
2020-09-17 17:26:27 +0800

    add test code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String[] arg = {"-i", "D:/"};
-        Run run = new Run(arg);
-        arg = new String[]{"-u", "rspt", "-e", "PushEvent"};
-        run.analyse(arg);
-        arg = new String[]{"-r", "izuzero/xe-module-ajaxboard", "-e", "PushEvent"};
-        run.analyse(arg);
-        arg = new String[]{"u", "zhouzhi2015", "-r", "zhouzhi2015/temp", "-e", "PushEvent"};
-        run.analyse(arg);
+//        String[] arg = {"-i", "D:/"};
+//        Run run = new Run(arg);
+//        arg = new String[]{"-u", "rspt", "-e", "PushEvent"};
+//        run.analyse(arg);
+//        arg = new String[]{"-r", "izuzero/xe-module-ajaxboard", "-e", "PushEvent"};
+//        run.analyse(arg);
+//        arg = new String[]{"u", "zhouzhi2015", "-r", "zhouzhi2015/temp", "-e", "PushEvent"};
+//        run.analyse(arg);
 //        System.out.println("Hello World");
-//        Run run = new Run(args);
+        Run run = new Run(args);
     }
 }
